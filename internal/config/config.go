b3e3e3d9f57aea0f480906553c141e204fac42e3
Thiago S Adriano <thiago.adriano26@gmail.com>
2025-01-14 21:45:16 -0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	NotionAPI     = "https://api.notion.com/v1"
-	DatabaseID    = "173bec2e54908006b4d5d777326550e3"
-	NotionToken   = "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"
+	DatabaseID    = "173bec2e54908006b4d5d77732655E_EXEMPLO"
+	NotionToken   = "ntn_512070995369osCHdMpMORLOqOUVMF8R8L5NBZ6A6hr_EXEMPLO"
 	NotionVersion = "2022-06-28"
 )
